dcae1b73af04155bd789d1bf6e929cf21b7f76c1
Daniel2000k <dto102001@gmail.com>
2025-11-29 13:20:59 -0500

    vercel video

--- api/index.py
+++ api/index.py
@@ -1,3 +1,13 @@
-from mytienda.asgi import application as app
+import sys
+import os
 
-# Vercel busca 'app' como entrypoint
+# ruta absoluta del proyecto
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# agrega backend al sys.path
+BACKEND_PATH = os.path.join(BASE_DIR, "backend")
+if BACKEND_PATH not in sys.path:
+    sys.path.append(BACKEND_PATH)
+
+# ahora sí importa Django
+from mytienda.asgi import application as app
